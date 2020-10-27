d95e592641d18ea81367fce79fcd941f3b1c9c49
yudongyue <oakyue@outlook.com>
2020-10-27 22:07:14 +0800

    添加@ImportResource 加载beans.xml配置文件

--- springboot-properties/src/main/java/com/example/springbootproperties/SpringbootPropertiesApplication.java
+++ springboot-properties/src/main/java/com/example/springbootproperties/SpringbootPropertiesApplication.java
@@ -2,8 +2,10 @@ package com.example.springbootproperties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = {"classpath:beans.xml"})
 public class SpringbootPropertiesApplication {
 
     public static void main(String[] args) {
