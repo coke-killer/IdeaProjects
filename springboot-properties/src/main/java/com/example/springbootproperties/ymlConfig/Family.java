54051ccbcca70b92a4ba78970a383471024dc8e4
yudongyue <yudongyue@daqo.com>
2020-10-27 17:00:02 +0800

    验证@NotNull

--- springboot-properties/src/main/java/com/example/springbootproperties/ymlConfig/Family.java
+++ springboot-properties/src/main/java/com/example/springbootproperties/ymlConfig/Family.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.util.Map;
 
 @Data
@@ -16,6 +17,7 @@ public class Family {
     //@Value获取yml文件的值,不支持松散绑定,一次获取一个值，支持spel,${}表达式,不支持数据校验和复杂类型的数组。
 //    @Value("${family.family-name}")
     private String familyName;
+    @NotNull
     private Map<String, String> map;
     private Father father;
     private Mother mother;
