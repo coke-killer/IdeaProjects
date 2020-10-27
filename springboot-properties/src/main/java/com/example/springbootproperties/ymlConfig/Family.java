26dc1b289a01662a241b60a6e091ffd40de61907
yudongyue <yudongyue@daqo.com>
2020-10-27 16:31:50 +0800

    增加@Value  不同于@ConfigurationProperties注释

--- springboot-properties/src/main/java/com/example/springbootproperties/ymlConfig/Family.java
+++ springboot-properties/src/main/java/com/example/springbootproperties/ymlConfig/Family.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Component
 @ConfigurationProperties(prefix = "family")
 public class Family {
-    //@Value获取yml文件的值,不支持松散绑定,一次获取一个值
+    //@Value获取yml文件的值,不支持松散绑定,一次获取一个值，支持spel,${}表达式,不支持数据校验和复杂类型的数组。
 //    @Value("${family.family-name}")
     private String familyName;
     private Map<String, String> map;
