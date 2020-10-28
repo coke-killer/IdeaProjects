035f558690d2caed7cdd153d69edf715c5390373
yudongyue <yudongyue@daqo.com>
2020-10-28 13:11:26 +0800

    添加family测试类

---
+++ springboot-properties/src/test/java/com/example/springbootproperties/ymlConfig/FamilyTest.java
@@ -0,0 +1,29 @@
+package com.example.springbootproperties.ymlConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@EnableConfigurationProperties
+class FamilyTest {
+    @Autowired
+    Family family;
+    @Autowired
+    ConfigurableApplicationContext ioc;
+
+    @Test
+    public void test() {
+        System.out.println(family.toString());
+    }
+
+    @Test
+    public void testBean() {
+        System.out.println(ioc.containsBean("testImportResourceService"));
+    }
+}
