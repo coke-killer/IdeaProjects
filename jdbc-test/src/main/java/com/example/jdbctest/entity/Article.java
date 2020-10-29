7853096a5fd8e54c6857edcbec47e867a280afe2
yudongyue <yudongyue@daqo.com>
2020-10-29 17:05:57 +0800

    新增

--- jdbc-test/src/main/java/com/example/jdbctest/entity/Article.java
+++ jdbc-test/src/main/java/com/example/jdbctest/entity/Article.java
@@ -2,12 +2,14 @@ package com.example.jdbctest.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Article {
