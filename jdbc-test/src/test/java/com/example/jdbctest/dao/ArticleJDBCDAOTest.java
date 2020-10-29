337a7d7b9f3801a89986b14163e3aa1e314feaeb
yudongyue <yudongyue@daqo.com>
2020-10-29 10:28:22 +0800

    添加所有测试方法

--- jdbc-test/src/test/java/com/example/jdbctest/dao/ArticleJDBCDAOTest.java
+++ jdbc-test/src/test/java/com/example/jdbctest/dao/ArticleJDBCDAOTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,23 +17,31 @@ class ArticleJDBCDAOTest {
 
     @Test
     void save() {
-        Article article = new Article("zzz", "10月29心情日记", "xxxxxxxxx", new Date());
+        Article article = new Article("小王", "10月29心情日记", "xxxxxxxxx", new Date());
         articleJDBCDAO.save(article);
     }
 
     @Test
     void deleteArticleById() {
+        articleJDBCDAO.deleteArticleById(1l);
     }
 
     @Test
     void updateById() {
+        Article article = new Article(2l, "小鱼", "10月29心情日记", "xxxxxxxxx", new Date());
+        articleJDBCDAO.updateById(article);
+
     }
 
     @Test
     void findArticleById() {
+        Article articleById = articleJDBCDAO.findArticleById(2l);
+        System.out.println(articleById);
     }
 
     @Test
     void findArticle() {
+        List<Article> article = articleJDBCDAO.findArticle();
+        System.out.println(article.toString());
     }
 }
